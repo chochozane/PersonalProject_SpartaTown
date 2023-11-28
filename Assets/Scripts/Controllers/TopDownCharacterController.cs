ee36bd3ca6aa8c8f7bb1bcdec0760d338df1205b
ZANE <130233619+chochozane@users.noreply.github.com>
2023-11-28 16:17:34 +0900

    [ADD] Add Package
    add package Input System
    and edit TopDownCharacterController.cs (for using package)

--- Assets/Scripts/Controllers/TopDownCharacterController.cs
+++ Assets/Scripts/Controllers/TopDownCharacterController.cs
@@ -1,23 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownCharacterController : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    // event 는 외부에서는 호출하지 못하게 막아주는 기능 !
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
 
-    // Start is called before the first frame update
-    void Start()
+    public void CallMoveEvent(Vector2 direction)
     {
-        
+        OnMoveEvent?.Invoke(direction);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void CallLookEvent(Vector2 direction)
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        transform.position += new Vector3(x, y) * speed * Time.deltaTime;
+        OnLookEvent?.Invoke(direction);
     }
 }
+
+
+    // 이동 방법 (간단 ver.)
+    ////[SerializeField] private float speed = 5f;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //float x = Input.GetAxisRaw("Horizontal");
+    //    //float y = Input.GetAxisRaw("Vertical");
+
+    //    //transform.position += new Vector3(x, y) * speed * Time.deltaTime;
+    //}
